ccb274edb8a18cf67a0bc9ffc25626a176e5cede
Antoine Yi-Cheng Tian <antoine186@hotmail.com>
2020-07-28 04:59:29 +0100

    Overhauled entire architecture for Cancer Images

--- AutoEncode.py
+++ AutoEncode.py
@@ -1,17 +1,17 @@
-from keras.layers import Input, Dense, Lambda
+from keras.layers import Input, Dense, Lambda, Conv2D, Reshape, MaxPool2D,\
+    Flatten, Conv2DTranspose, UpSampling2D
 from keras.models import Model
+from keras.metrics import categorical_accuracy
+from keras.metrics import binary_crossentropy
 from keras import backend as K
 from keras import objectives
 from keras.datasets import mnist
 from sklearn.model_selection import train_test_split
 import numpy as np
 
-# !!! Plagiarism disclaimer !!!
-# A large portion of the code below was taken from the book GANs in Action by Jakub Langr and Vladimir Bok.
-# I have retrofitted and modified those for my own purpose.
-
 def autoencode_fit(X, Y, batch_size, original_dim, latent_dim, intermediate_dim, nb_epoch):
 
+    unflat_dim = original_dim
     original_dim = original_dim[0] * original_dim[1]
 
     def sampling(args):
@@ -21,17 +21,54 @@ def autoencode_fit(X, Y, batch_size, original_dim, latent_dim, intermediate_dim,
 
     # This is the encoder part / Third output is our z latent variable.
     x = Input(shape=(original_dim,), name="input")
-    h = Dense(intermediate_dim, activation="relu", name="encoding")(x)
-    z_mean = Dense(latent_dim, name="mean")(h)
-    z_log_var = Dense(latent_dim, name="log-variance")(h)
-    z = Lambda(sampling, output_shape=(latent_dim,))([z_mean, z_log_var])
+    print(x)
+    x2 = Reshape((unflat_dim[0], unflat_dim[1], 1), name="x2")(x)
+    print(x2)
+    h1 = Conv2D(4, kernel_size=(5, 5), activation='relu',
+                batch_input_shape=(batch_size, unflat_dim[0], unflat_dim[1], 1), name="h1")(x2)
+    print(h1)
+    h2 = MaxPool2D(pool_size=(2, 2), strides=None, padding="valid", data_format=None,
+                   name="h2")(h1)
+    print(h2)
+    h3 = Conv2D(4, kernel_size=(5, 5), activation='relu', name="h3")(h2)
+    print(h3)
+    h4 = MaxPool2D(pool_size=(2, 2), strides=None, padding="valid", data_format=None,
+                   name="h4")(h3)
+    print(h4)
+    h5 = Dense(intermediate_dim, activation="relu", name="h5")(h4)
+    print(h5)
+    h6 = Flatten(name="h6")(h5)
+    print(h6)
+    z_mean = Dense(latent_dim, name="mean")(h6)
+    z_log_var = Dense(latent_dim, name="log-variance")(h6)
+    print(z_mean)
+    print(z_log_var)
+    z = Lambda(sampling, output_shape=(latent_dim,), name="z")([z_mean, z_log_var])
+    print(z)
     encoder = Model(x, [z_mean, z_log_var, z], name="encoder")
+    print("Model Switch")
 
     # This is the decoder part / Output is a flattened image.
     input_decoder = Input(shape=(latent_dim,), name="decoder_input")
-    decoder_h = Dense(intermediate_dim, activation="relu", name="decoder_h")(input_decoder)
-    x_decoded = Dense(original_dim, activation="softmax", name="flat_decoded")(decoder_h)
-    decoder = Model(input_decoder, x_decoded, name="decoder")
+    print(input_decoder)
+    input_decoder2 = Dense(intermediate_dim, activation="relu", name="input_decoder2")(input_decoder)
+    print(input_decoder2)
+    input_decoder3 = Reshape((16, 16, 1), name="input_decoder3")(input_decoder2)
+    print(input_decoder3)
+    q1 = Conv2DTranspose(4, kernel_size=(5, 5), activation='relu', name="q1")(input_decoder3)
+    print(q1)
+    #u_q1 = UpSampling2D((2, 2), name="u_q1")(x)
+    #print(u_q1)
+    q2 = Conv2DTranspose(4, kernel_size=(5, 5), activation='relu', name="q2")(q1)
+    print(q2)
+    #u_q2 = UpSampling2D((2, 2), name="u_q2")(x)
+    #print(u_q2)
+    x_decoded0 = Flatten()(q2)
+    x_decoded = Dense(unflat_dim[0] * unflat_dim[1], activation="softmax", name="flat_decoded")(x_decoded0)
+    print(x_decoded)
+    x_decoded2 = Reshape((original_dim,), name="reshaped")(x_decoded)
+    print(x_decoded2)
+    decoder = Model(input_decoder, x_decoded2, name="decoder")
 
     output_combined = decoder(encoder(x)[2])
     vae = Model(x, output_combined)
@@ -44,6 +81,7 @@ def autoencode_fit(X, Y, batch_size, original_dim, latent_dim, intermediate_dim,
         kl_loss = -0.5 * K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
         return xent_loss + kl_loss
 
+    #vae.compile(optimizer="adam", loss=binary_crossentropy)
     vae.compile(optimizer="adam", loss=vae_loss)
 
     # We use the MNIST dataset for debugging purposes
@@ -55,6 +93,9 @@ def autoencode_fit(X, Y, batch_size, original_dim, latent_dim, intermediate_dim,
     # Flattening on a per sample basis.
     x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
     x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+
+    print(x_train.shape)
+    print(x_test.shape)
 
     vae.fit(x_train, x_train,
             shuffle=True,
--- Main_File.py
+++ Main_File.py
@@ -1,10 +1,16 @@
 from AutoEncode import autoencode_fit
 from Synth_Print import synth_print
 from Image_Loader import fast_slides_select_mag
+from Image_Loader import all_slides_select_mag
 from RGB_2_Gray import r2g
 from keras.datasets import mnist
 import numpy as np
+import os
 
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]="-1"
+
+"""
 # Apply VAE with the mnist dataset
 batch_size = 100
 original_dim = (28, 28)
@@ -19,28 +25,48 @@ Y = np.concatenate((y_train, y_test))
 
 encoder, decoder, vae = autoencode_fit(X, Y, batch_size, original_dim, latent_dim, intermediate_dim, nb_epoch)
 synth_print(nb_fig, original_dim, decoder)
+"""
 
 # Apply VAE with the BreaKHis dataset // This section sometimes experiences exponentially increasing gradient
 batch_size = 10
 original_dim = (460, 700)
 latent_dim = 2
 intermediate_dim = 256
-nb_epoch = 5
-nb_fig = 5
+nb_epoch = 1
+nb_fig = 15
 
 # We are only using one possible condition in order to test our vae
-all_slides_mag, all_slides_mag_labels, le = fast_slides_select_mag("40X")
+#all_slides_mag, all_slides_mag_labels, le = fast_slides_select_mag("40X")
+#all_slides_mag, all_slides_mag_labels, le = all_slides_select_mag("40X")
+all_slides_mag = np.load("D:\\GitHub Projects\\Python Based\\Neural Network\\Datasets\\BreaKHis_Cube\\Cube.npy")
+all_slides_mag_labels = np.load("D:\\GitHub Projects\\Python Based\\Neural Network\\Datasets\\BreaKHis_Cube\\Labels.npy")
 
-# Balancing samples for batch size
-all_slides_mag = all_slides_mag[0:100,:,:,:]
-all_slides_mag_labels = all_slides_mag_labels[0:100]
+all_slides_mag = all_slides_mag[0:1900,:,:]
+all_slides_mag_labels = all_slides_mag_labels[0:1900]
+
+# Balancing samples for batch size / Only when using fast loading
+#all_slides_mag = all_slides_mag[0:100,:,:,:]
+#all_slides_mag_labels = all_slides_mag_labels[0:100]
+
+"""
+# Balancing samples for batch size / Only when using full loading
+all_slides_mag = all_slides_mag[0:1970,:,:,:]
+all_slides_mag_labels = all_slides_mag_labels[0:1970]
 
 # Converting RGB to grayscale image
 all_slides_mag = r2g(all_slides_mag)
 
+# Save/loading gray cube and labels
+np.save("D:\\GitHub Projects\\Python Based\\Neural Network\\Datasets\\BreaKHis_Cube\\Cube.npy", all_slides_mag)
+np.save("D:\\GitHub Projects\\Python Based\\Neural Network\\Datasets\\BreaKHis_Cube\\Labels.npy", all_slides_mag_labels)
+"""
+
 # Fitting our VAE
 encoder, decoder, vae = autoencode_fit(all_slides_mag, all_slides_mag_labels, batch_size, original_dim, latent_dim,
                                        intermediate_dim, nb_epoch)
+
+encoder.save("D:\\GitHub Projects\\Python Based\\Neural Network\\AutoEncoder\\encoder")
+decoder.save("D:\\GitHub Projects\\Python Based\\Neural Network\\AutoEncoder\\decoder")
 
 # Displaying a possible subset of synthetic Cancer Images
 synth_print(nb_fig, original_dim, decoder)
