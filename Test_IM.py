bda2884b8a9cba4f3903bf32566b8056fcbe7d66
Antoine Yi-Cheng Tian <antoine186@hotmail.com>
2020-07-28 16:28:07 +0100

    Fixed test printing

--- Test_IM.py
+++ Test_IM.py
@@ -1,9 +1,14 @@
+from scipy.stats import norm
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 
 def test_im(decoder, original_dim):
-    z = np.random.randint(0.05, 0.95, 256)
-    raw_pred = decoder.predict(z)
-    full_pred = raw_pred.reshape(original_dim)#
+    z = np.zeros(256)
+    for i in range(256):
+        z[i] = norm.ppf(random.uniform(0.05, 0.95))
+    z = z.tolist()
+    raw_pred = decoder.predict([z])
+    full_pred = raw_pred.reshape(original_dim)
     plt.imshow(full_pred, cmap='Greys_r')
     plt.show()
